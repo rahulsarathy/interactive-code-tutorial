8f70729c05a2e75b7930a98ebaa2aa622abb36b2
Rahul Sarathy <rahul@sarathy.org>
2020-05-21 11:46:23 -0700

    sublime like theme

--- src/components/code_preview.tsx
+++ src/components/code_preview.tsx
@@ -3,12 +3,14 @@ import "./code_preview.scss";
 
 import { CodeView } from "./code_view";
 import { Preview } from "./preview";
+import { FileNames } from "./filenames";
 
 export class CodePreview extends Component {
 	render() {
 		return (
 			<div className="code_preview">
 				<Preview />
+				<FileNames />
 				<CodeView />
 			</div>
 		);
--- src/components/code_view.tsx
+++ src/components/code_view.tsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
+import okaidia from "react-syntax-highlighter/dist/esm/styles/prism/okaidia";
 import "./code_view.scss";
+import { codeString } from "../code_string";
 
-import { FileNames } from "./filenames";
+SyntaxHighlighter.registerLanguage("jsx", jsx);
 
 export class CodeView extends Component {
 	render() {
 		return (
 			<div className="code_view">
-				<FileNames />
+				<SyntaxHighlighter
+					showLineNumbers={true}
+					language="jsx"
+					style={okaidia}
+				>
+					{codeString}
+				</SyntaxHighlighter>
 			</div>
 		);
 	}
